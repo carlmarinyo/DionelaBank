410a7ab510e5380b15db8912c6983e6b5750936e
korkydoobie <202310065@fit.edu.ph>
2024-11-16 19:29:41 +0800

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Library {
     private LibraryItem availablebooks[];
     private int count; //pang limit ng size ng array tsaka pang loop 
@@ -30,6 +32,45 @@ public void addItem(LibraryItem item) { //mag aadd ng either book,dvd,magazine s
         for (int i = 0; i < count; i++) {
             availablebooks[i].displayInfo();
             System.out.print("\n\n");
+        }
+    }
+
+    public void returnBook(){
+        Scanner input = new Scanner(System.in);
+        boolean more = true;
+
+        while(more){ //habang true -- loop
+            System.out.println("Enter User ID: ");
+            String userId = input.nextLine();
+
+            System.out.println("Enter Item ID to be returned: ");
+            int itemId = input.nextInt();
+            input.nextLine();
+
+
+            boolean found = false;
+            for (int i = 0; i < count; i++){
+                if (availablebooks[i].getItemId == itemId){
+                    found = true;
+                    availablebooks[i].increaseQuantity();
+                    System.out.println("\"" + availablebooks[i].getTitle() + "\" has been returned");
+                    break;
+                }
+            }
+
+            if (!found){
+                System.out.println("Item with ID " + itemId + "not found");
+            }
+
+            System.out.println("Return more? (y/n): ");
+            char response = input.nextLine().charAt(0);
+
+            if (response == 'y' || response == 'Y'){
+                more = true;
+            }
+            else{
+                more = false;
+            }
         }
     }
 
